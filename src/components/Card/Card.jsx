b5edf24f1c0aa3d053b89995f8d70fbd06aba017
Ravikiran KVS <kvsravikiran16@gmail.com>
2026-06-10 17:17:22 +0530

    feat: complete Day 3 Card component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,5 +1,21 @@
-import './Card.css';
+import "./Card.css";
 
-export const Card = () => {
-  return <div></div>;
+export const Card = ({ children, ...props }) => {
+  return (
+    <div className="card" {...props}>
+      {children}
+    </div>
+  );
+};
+
+Card.Header = ({ children }) => {
+  return <div className="card-header">{children}</div>;
+};
+
+Card.Body = ({ children }) => {
+  return <div className="card-body">{children}</div>;
+};
+
+Card.Footer = ({ children }) => {
+  return <div className="card-footer">{children}</div>;
 };
